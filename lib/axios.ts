1a22e10ce8cd0b5c5dc97f18fcfa874456733481
dubey-supriya-270 <dubeysupriya270@gmail.com>
2025-03-30 21:27:59 +0530

    axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,14 +1,8 @@
 // lib/axios.ts
 import axios from 'axios'
 
-const baseURL =
-  typeof window === 'undefined'
-    ? process.env.NEXT_PUBLIC_API_URL || `http://localhost:3000`
-    : window.location.origin
 
 const api = axios.create({
-  baseURL,
-  withCredentials: true, // optional if you’re handling cookies
-})
-
+    baseURL: process.env.NEXT_PUBLIC_API_URL 
+  });
 export default api
